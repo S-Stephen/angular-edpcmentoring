acb6e5826bca229c68d85549213bbc1b73fa64c1
S-Stephen <stephen.shorrock@gmail.com>
2019-09-30 21:50:20 +0100

    remove modernizr (debug)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -214,7 +214,7 @@ export class AppComponent {
 
         //set namespaced variable to determine layout of menu
         //using modernizr to detect if media query is valid and has been triggered
-        if(Modernizr.mq('only screen and (max-width: 767px)')){
+        /if(Modernizr.mq('only screen and (max-width: 767px)')){
           projectlight.mobileLayout  = true;
           
           //call function to remove uniform column height in footers for mobile layout
@@ -233,7 +233,7 @@ export class AppComponent {
           //projectlight.setSectionListChildrenColumnHeight();
           //projectlight.setContentColumnHeight();
           //projectlight.setFooterColumnsHeight();
-        }*/
+        }
         
     });
   }
