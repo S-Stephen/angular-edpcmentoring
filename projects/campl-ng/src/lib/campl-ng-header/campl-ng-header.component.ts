23c42838063a7feb9c62d8474cd41a8497450782
StephenS <Stephen.shorrock@gmail.com>
2019-10-15 16:16:45 +0100

    Append th equicklinks to the mobile navigation menu

--- projects/campl-ng/src/lib/campl-ng-header/campl-ng-header.component.ts
+++ projects/campl-ng/src/lib/campl-ng-header/campl-ng-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, Renderer2 } from "@angular/core";
-import { CamplNgPrimaryMenuStateService } from '../services/campl-ng-primary-menu-state.service';
+import { CamplService } from "../services/campl.service";
+import { CamplNgPrimaryMenuStateService } from "../services/campl-ng-primary-menu-state.service";
 
 @Component({
   selector: "campl-ng-header",
@@ -9,6 +10,9 @@ import { CamplNgPrimaryMenuStateService } from '../services/campl-ng-primary-men
 export class CamplNgHeaderComponent implements OnInit, OnDestroy {
   menu_open: boolean = false;
   search_open: boolean = false;
+  public config: any;
+  public quicklinks: any;
+
   // there is opportunity here to create a new class/interface with this field as reflection and the injection of the MenuService
   //  BUT we would have to manage the search boxes as a seperate component! - trouble the responsive box appears in a differrent
   //  componant as the regular box - communication required?
@@ -16,23 +20,27 @@ export class CamplNgHeaderComponent implements OnInit, OnDestroy {
   public myidsearch: string = "CamplNgHeaderComponentSearch";
   public myidnav: string = "CamplNgHeaderComponentNav";
 
-  constructor(public primary_comp: CamplNgPrimaryMenuStateService, private renderer: Renderer2) {}
+  constructor(
+    public primary_comp: CamplNgPrimaryMenuStateService,
+    public campl_config: CamplService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit() {
+    this.config = this.campl_config.getConfig();
+    this.quicklinks = this.config.quicklinks;
     this.primary_comp.id$.subscribe(id => {
-        if (id == this.myidsearch && !this.search_open)
-            this.search_open=true
-        else
-            this.search_open=false
+      if (id == this.myidsearch && !this.search_open) this.search_open = true;
+      else this.search_open = false;
 
-        if (id == this.myidnav && !this.menu_open){
-            this.menu_open = true
-            this.openBodyNav()
-        }else if(this.menu_open){
-            this.menu_open = false
-            this.closeBodyNav()
-        }
-    })
+      if (id == this.myidnav && !this.menu_open) {
+        this.menu_open = true;
+        this.openBodyNav();
+      } else if (this.menu_open) {
+        this.menu_open = false;
+        this.closeBodyNav();
+      }
+    });
   }
 
   ngOnDestroy() {
@@ -45,18 +53,18 @@ export class CamplNgHeaderComponent implements OnInit, OnDestroy {
     this.renderer.removeClass(document.body, "campl-navigation-open");
   }
 
-  openBodyNav(){
+  openBodyNav() {
     this.renderer.addClass(document.body, "campl-navigation-open");
   }
 
   toggleBodyNav() {
-    let id: string = (!this.menu_open)? this.myidnav : "NOTSET";
+    let id: string = !this.menu_open ? this.myidnav : "NOTSET";
     this.primary_comp.sendId(id);
   }
 
   toggleSearch() {
-      console.log("toggle search")
-    let id: string = (!this.search_open)? this.myidsearch : "NOTSET";
+    console.log("toggle search");
+    let id: string = !this.search_open ? this.myidsearch : "NOTSET";
     this.primary_comp.sendId(id);
   }
 }
