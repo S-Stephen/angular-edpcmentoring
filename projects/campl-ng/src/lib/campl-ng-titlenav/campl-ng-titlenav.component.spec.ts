5fe029690ab969628a584157f1eb0204ad855636
StephenS <Stephen.shorrock@gmail.com>
2019-10-30 15:10:15 +0000

    create component

--- projects/campl-ng/src/lib/campl-ng-titlenav/campl-ng-titlenav.component.spec.ts
+++ projects/campl-ng/src/lib/campl-ng-titlenav/campl-ng-titlenav.component.spec.ts
@@ -2,10 +2,11 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { Component, Input } from "@angular/core";
 import { CamplNgTitlenavComponent } from "./campl-ng-titlenav.component";
 import { NavMenu } from "../models/nav-menu";
-import { Observable, of } from "rxjs";
+import { Observable, of, ReplaySubject } from "rxjs";
 import { CamplService } from "../services/campl.service";
 import { CamplConfigService } from "../services/campl-config.service";
 import { CamplNgNavComponent } from "../campl-ng-nav/campl-ng-nav.component";
+import { CamplNgCapabilitiesService } from "../services/campl-ng-capabilities.service";
 
 /**
  *  @Component({
@@ -19,25 +20,44 @@ class CamplNgNavComponent {
 }
 */
 
+@Component({
+  selector: "campl-ng-localnav-menu",
+  template: "<p>Mock localnav</p>"
+})
+class MocklocalNavComponent {
+  @Input() menu: any;
+}
+
 describe("CamplNgTitlenavComponent", () => {
   let component: CamplNgTitlenavComponent;
   let fixture: ComponentFixture<CamplNgTitlenavComponent>;
   let camplConfig: any = {};
+  let fakeCapabilities;
+  let capSubject = new ReplaySubject(1);
 
   beforeEach(async(() => {
+    fakeCapabilities = {
+      modernizrSource: capSubject.asObservable()
+    };
     TestBed.configureTestingModule({
-      declarations: [CamplNgTitlenavComponent, CamplNgNavComponent],
+      declarations: [
+        CamplNgTitlenavComponent,
+        CamplNgNavComponent,
+        MocklocalNavComponent
+      ],
       providers: [
         CamplService,
         {
           provide: CamplConfigService,
           useValue: camplConfig
-        }
+        },
+        { provide: CamplNgCapabilitiesService, useValue: fakeCapabilities }
       ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
+    capSubject.next({ supported: true });
     fixture = TestBed.createComponent(CamplNgTitlenavComponent);
     component = fixture.componentInstance;
     // this needs to be set so the template can pass it 'down'
