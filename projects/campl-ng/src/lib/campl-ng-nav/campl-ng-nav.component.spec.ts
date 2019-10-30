e9fdd504b323abe4eaee8aaf9bb9cea6ad8a224d
StephenS <Stephen.shorrock@gmail.com>
2019-10-30 15:01:40 +0000

    creates the component

--- projects/campl-ng/src/lib/campl-ng-nav/campl-ng-nav.component.spec.ts
+++ projects/campl-ng/src/lib/campl-ng-nav/campl-ng-nav.component.spec.ts
@@ -1,24 +1,44 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CamplNgNavComponent } from "./campl-ng-nav.component";
+import { CamplNgLocalnavMenuComponent } from "../campl-ng-localnav-menu/campl-ng-localnav-menu.component";
 
-import { of } from "rxjs";
+import { of, ReplaySubject } from "rxjs";
 import { NavMenu } from "../models/nav-menu";
+import { CamplNgCapabilitiesService } from "../services/campl-ng-capabilities.service";
+import { Component, Input } from "@angular/core";
 
-describe("CamplNgNavComponent", () => {
+@Component({
+  selector: "campl-ng-localnav-menu",
+  template: "<p>Mock localnav</p>"
+})
+class MocklocalNavComponent {
+  @Input() menu: any;
+}
+
+fdescribe("CamplNgNavComponent", () => {
   let component: CamplNgNavComponent;
   let fixture: ComponentFixture<CamplNgNavComponent>;
+  let fakeCapabilities;
+  let capSubject = new ReplaySubject(1);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [CamplNgNavComponent]
-    }).compileComponents();
-  }));
+  beforeEach(async(() => {}));
 
   beforeEach(() => {
+    fakeCapabilities = {
+      modernizrSource: capSubject.asObservable()
+    };
+    TestBed.configureTestingModule({
+      declarations: [CamplNgNavComponent, MocklocalNavComponent],
+      providers: [
+        { provide: CamplNgCapabilitiesService, useValue: fakeCapabilities }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CamplNgNavComponent);
     component = fixture.componentInstance;
 
+    capSubject.next({ supported: true });
     // mock an input to the component
     let navm = {} as NavMenu;
     component.nav_menu$ = of(navm);
