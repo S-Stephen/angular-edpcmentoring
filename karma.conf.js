c3d8c75268a293d5204d066bb31245a0d072490e
StephenS <Stephen.shorrock@gmail.com>
2019-11-01 14:43:14 +0000

    notes added

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
+// custom launcher: https://medium.com/faun/configuring-travis-ci-for-angular-application-34afee1715f
+
 module.exports = function (config) {
   config.set({
     basePath: '',
