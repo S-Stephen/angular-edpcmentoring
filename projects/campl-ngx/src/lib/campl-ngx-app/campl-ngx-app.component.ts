8a9d3a34cb711388d6d23af10d1b71406588064f
StephenS <Stephen.shorrock@gmail.com>
2019-11-04 11:19:19 +0000

    tidy

--- projects/campl-ngx/src/lib/campl-ngx-app/campl-ngx-app.component.ts
+++ projects/campl-ngx/src/lib/campl-ngx-app/campl-ngx-app.component.ts
@@ -3,21 +3,17 @@ import { NavMenu } from "../models/nav-menu";
 import { Observable } from "rxjs/Observable";
 import { CamplNgxCapabilitiesService } from "../services/campl-ngx-capabilities.service";
 
-// this has been included via a script tag
-// declare var project_light_cued: any;
-
 // include the style as part of the library:
 // https://stackoverflow.com/questions/51065359/include-assets-in-your-angular-6-library
 @Component({
   selector: "campl-ngx-app",
   templateUrl: "./campl-ngx-app.component.html",
-  //styles: ["h1 { background-color: red }"]
-  //styleUrls: ["./campl-ngx-app.component.css"],
   styleUrls: [
+    // The slightly modified full css include base64 images and a couple minor hierarchical changes
     "./full-stylesheet-base64.css",
     "./campl-ngx-app.component-custom.css"
   ],
-  encapsulation: ViewEncapsulation.None // required to provide style to others
+  encapsulation: ViewEncapsulation.None // required to provide style to others (entire page)
 })
 export class CamplNgxComponent implements OnInit {
   // TODO given this input can we generate a service that we can inject
